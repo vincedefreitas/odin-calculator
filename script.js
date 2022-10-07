750e73d53e47c1b22a703e7016307542723c8d59
Vince <vince.defreitas@gmail.com>
2022-10-07 12:45:12 +0100

    Create calculator buttons and link css stylesheet

--- script.js
+++ script.js
@@ -14,9 +14,17 @@ function divide(a, b) {
     return a / b;
 }
 
-let operate = {
+let operators = {
     '+': add,
     '-': subtract,
     '*': multiply,
     '/': divide,
 }
+
+function operate(operator, num1, num2) {
+    if (operator in operators) {
+        return operators[operator](num1, num2);
+    }
+}
+
+
